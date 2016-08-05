9b9ee3a38bd3e96033c44f8e21ccd5e24c3ad6a7
ralejotuanama <Sistemas++2015**>
2016-08-05 09:03:41 -0500

    novedades
    noveades123

---
+++ novedades.php
@@ -0,0 +1,83 @@
+<?php
+require_once "header.php";
+?>
+<!-- CONTENIDO: INICIO -->
+<div class="imgmain">
+	<img src="images/productoimg1.jpg">
+	<div class="textoimgmain">
+		<h2>Novedades</h2>
+		<h5>Phasellus imperdiet mi non eros maximus eleifend.</h5>
+	</div>
+</div>
+<div class='shadow-bottom sb1'></div>
+<div class='container thin thin5'>
+	<div class="contenedornovedades">
+		<div class="col-md-12 col-sm-6 ">
+			<div class='product-block prodprodblock prodprodblockgal imgnovprincipal'>
+				<a href="images/gallery/0003.jpg"><img src="images/gallery/0003.jpg" /><div class="galimgtitle-bg"><img src="images/title-bg.png"></div></a>
+				<div class='product-desc product-descnovedades'>
+					<strong>Curabitur et orci ornare</strong>
+					<p>Fusce ac tristique sodales</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 col-sm-6">
+			<div class='product-block prodprodblock prodprodblockgal'>
+				<a href="images/gallery/0003.jpg"><img src="images/gallery/0003.jpg" /><div class="galimgtitle-bg"><img src="images/title-bg.png"></div></a>
+				<div class='product-desc product-descnovedades'>
+					<strong>Curabitur et orci ornare</strong>
+					<p>Fusce ac tristique sodales</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 col-sm-6">
+			<div class='product-block prodprodblock prodprodblockgal'>
+				<a href="images/gallery/0002.jpg"><img src="images/gallery/0002.jpg" /><div class="galimgtitle-bg"><img src="images/title-bg.png"></div></a>
+				<div class='product-desc product-descnovedades'>
+					<strong>Curabitur et orci ornare</strong>
+					<p>Fusce ac tristique sodales</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 col-sm-6">
+			<div class='product-block prodprodblock prodprodblockgal'>
+				<a href="images/gallery/0001.jpg"><img src="images/gallery/0001.jpg" /><div class="galimgtitle-bg"><img src="images/title-bg.png"></div></a>
+				<div class='product-desc product-descnovedades'>
+					<strong>Curabitur et orci ornare</strong>
+					<p>Fusce ac tristique sodales</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 col-sm-6">
+			<div class='product-block prodprodblock prodprodblockgal'>
+				<a href="images/gallery/0002.jpg"><img src="images/gallery/0002.jpg" /><div class="galimgtitle-bg"><img src="images/title-bg.png"></div></a>
+				<div class='product-desc product-descnovedades'>
+					<strong>Curabitur et orci ornare</strong>
+					<p>Fusce ac tristique sodales</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="paginationnovedades">
+		<nav>
+		  <ul class="pagination">
+		    <li><a href="#">1</a></li>
+		    <li><a href="#">2</a></li>
+		    <li><a href="#">3</a></li>
+		    <li><a href="#">4</a></li>
+		    <li><a href="#">5</a></li>
+		    <li><a href="#">></a></li>
+		    <li>
+		      <a href="#" aria-label="Next">
+		        <span aria-hidden="true">>></span>
+		      </a>
+		    </li>
+		  </ul>
+		</nav>
+	</div>
+	<div class='shadow-bottom sbnovedades'></div>
+</div>
+<!-- CONTENIDO: FIN    -->
+<?php
+require_once "footer.php";
+?>
