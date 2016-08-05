57adbe393bf95efb5af9d2f7a979af84b7ebdc03
ronald152515@gmail.com <Sistemas++2015**>
2016-08-05 08:54:50 -0500

    nuevo material
    descripcion nuevo materiall

---
+++ galerias.php
@@ -0,0 +1,338 @@
+<?php
+require_once "header.php";
+?>
+<!-- CONTENIDO: INICIO -->
+<div class="imgmain">
+	<img src="images/productoimg1.jpg">
+	<div class="textoimgmain">
+		<h2>Galerías</h2>
+		<h5>Phasellus imperdiet mi non eros maximus eleifend.</h5>
+	</div>
+</div>
+<div class='shadow-bottom sb1'></div>
+
+<div class='container thin thin4'>
+	<div class='title qs2'>fotos</div>
+	<div id='slider-imggallery' class='carousel slide carousel-fade' data-ride='carousel'>
+		<!-- CONTENIDO -->
+		<div class='carousel-inner' role='listbox'>
+			<div class='item active'>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0001.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0001.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0002.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0002.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0003.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0003.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0004.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0004.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class='item'>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0005.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0005.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0006.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0006.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0004.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0004.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0003.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0003.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class='item'>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0002.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0002.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0001.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0001.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0003.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0003.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0006.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0006.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class='item'>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0004.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0004.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0005.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0005.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0002.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0002.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<a href="images/gallery/0001.jpg">
+						<div class='product-block prodprodblock prodprodblockgal'>
+							<img src="images/gallery/0001.jpg" />
+							<div class='product-desc'>
+								<strong>Curabitur et orci ornare</strong>
+								<p>Fusce ac tristique sodales</p>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div id="cigal">
+			<ol class="carousel-indicators">
+			    <li data-target="#slider-imggallery" data-slide-to="0" class="active"></li>
+			    <li data-target="#slider-imggallery" data-slide-to="1"></li>
+			    <li data-target="#slider-imggallery" data-slide-to="2"></li>
+			    <li data-target="#slider-imggallery" data-slide-to="3"></li>
+			 </ol>
+		</div>
+	</div>
+	<div class='container-fluid galcontainer'>
+		<div class='title qs3'>Videos</div>
+		<div id='slider-videogallery' class='carousel slide carousel-fade' data-ride='carousel'>
+		<!-- CONTENIDO -->
+			<div class='carousel-inner' role='listbox'>
+				<div class='item active'>
+					<div class="col-md-6 col-sm-6" data-toggle="modal" data-target="#myModal">
+						<div class='product-block prodprodblock prodvidblockgal' id="galvid1">
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class='item'>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class='item'>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class='item'>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class='product-block prodprodblock prodvidblockgal'>
+							<a href="http://www.youtube.com/embed/8SbUC-UaAxE?autoplay=true" rel="video">
+								<div class="playVideo"><img src="images/play_video.png"></div>
+								<img src="images/vidgalimg.jpg" />
+								<div class='product-desc'>
+									<strong>Curabitur et orci ornare</strong>
+									<p>Fusce ac tristique sodales</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="cigal">
+				<ol class="carousel-indicators">
+				    <li data-target="#slider-videogallery" data-slide-to="0" class="active"></li>
+				    <li data-target="#slider-videogallery" data-slide-to="1"></li>
+				    <li data-target="#slider-videogallery" data-slide-to="2"></li>
+				    <li data-target="#slider-videogallery" data-slide-to="3"></li>
+				 </ol>
+			</div>
+		</div>
+	</div>
+	<div class='shadow-bottom sbprod'></div>
+</div>
+<!-- CONTENIDO: FIN    -->
+<?php
+require_once "footer.php";
+?>
